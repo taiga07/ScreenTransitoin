1fe974463054a27b7bd2aed1da05fa5af2fedc3f
taiga07 <taigatani@me.com>
2023-06-11 11:07:45 +0900

    画面遷移

---
+++ ModalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ModalViewController.swift
+//  ScreenTransition
+//
+//  Created by 谷太海 on 2023/06/11.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func bacButtonAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
